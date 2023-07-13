956ae9484763c0bc4638f343bb57fc12b0c0e430
Oladipo Victor <dipovictor07@gmail.com>
2023-07-13 16:55:58 -0700

    commit this

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+int main(void)
+{
+	char ch[53] = "Programming is like building a multilingual puzzle";
+
+	puts(ch);
+	return (0);
+}
