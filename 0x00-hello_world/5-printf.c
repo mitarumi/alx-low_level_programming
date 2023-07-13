917967710b6f7b75759935386fce5abb38befcfb
Oladipo Victor <dipovictor07@gmail.com>
2023-07-13 16:31:15 -0700

    commit this

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,11 @@
+/**
+ * main - Entry point
+ * 
+ * return 0
+ */
 #include <stdio.h>
 int main(void)
 {
-	printf( "with proper grammar, but the outcome is a piece of art,\n" );
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
