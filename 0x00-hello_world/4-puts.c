038fe7f7be4f748bb65356d114b4b4e11150f886
Oladipo Victor <dipovictor07@gmail.com>
2023-07-13 17:03:00 -0700

    commit this

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
 #include <stdio.h>
 int main(void)
 {
-	char ch[53] = "Programming is like building a multilingual puzzle";
+	char ch[53] = "\"Programming is like building a multilingual puzzle";
 
 	puts(ch);
 	return (0);
