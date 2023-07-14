bdbabb179d1a03a8a72a914135e0eec3d243af49
Oladipo Victor <dipovictor07@gmail.com>
2023-07-13 17:25:38 -0700

    commit this

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+#include <conio.h>
+int main(void)
+{
+	int integerType;
+	float floatType;
+	double doubleType;
+	char charType;
+	long int longintType;
+
+	printf("Size of a char: %ld byte(s)\n",sizeof(charType));
+	printf("Size of an int: %ld bytes(s)\n",sizeof(integerType));
+	printf("Size of a long int: %ld bytes(s)\n",sizeof(longintType));
+	printf("Size of a long long int: %ld bytes(s)\n",sizeof(doubleType));
+	printf("Size of a float: %ld bytes(s)\n",sizeof(floatType));
+
+	return (0);
+}	
