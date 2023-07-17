a246d31cf391a8a2034ed0e7c73b826c0262af05
Oladipo Victor <dipovictor07@gmail.com>
2023-07-17 15:41:00 -0700

    commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,13 +10,10 @@
 int main(void)
 {
     char alphabet;
-
     for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
-    { 
-        putchar(alphabet);
-
+    {
+	    putchar(alphabet);
     }
-
     putchar('\n');
     return (0);
 }
