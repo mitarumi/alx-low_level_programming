e35a5687fbb2c97945f3f13bc4dbd969e2e46081
Oladipo Victor <dipovictor07@gmail.com>
2023-07-17 15:52:48 -0700

    commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char a;
+
+	while (a <= 'Z')
+	{
+		putchar(a);
+
+		a++;
+	}
+
+	putchar('\n');
+	return (0);
+}
