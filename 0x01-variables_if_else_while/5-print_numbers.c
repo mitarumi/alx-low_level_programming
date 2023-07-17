81229dad226e2668845cd3e1378564afe44b0a3b
Oladipo Victor <dipovictor07@gmail.com>
2023-07-17 16:01:51 -0700

    commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+/**
+ * main - Entry point
+ * 
+ * Description: 'the program's description'
+ * 
+ * Return: Always 0 (Success)
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("0123456789\n");
+	return (0);
+}
