7558c90a91d46f619351b17f7748d50af7dabb2f
Oladipo Victor <dipovictor07@gmail.com>
2023-07-17 15:35:56 -0700

    commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,12 +9,10 @@
 
 int main(void)
 {
-    //declare a variable
     char alphabet;
 
     for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
-    {
-        // print the value of the variable 
+    { 
         putchar(alphabet);
 
     }
