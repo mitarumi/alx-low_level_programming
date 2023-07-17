370f48b413f10727e232b4187fa17c40eeac55d9
Oladipo Victor <dipovictor07@gmail.com>
2023-07-17 16:12:28 -0700

    commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+/**
+ * main - Entry point
+ * 
+ * Description: 'the program's description'
+ * 
+ * Return: Always 0 (Success)
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	char alphabet;
+	for (alphabet = 'z'; alphabet >= 'a' ; alphabet--)
+	{
+		putchar(alphabet);
+
+	}
+
+	putchar('\n');
+	return (0);
+
+}	
