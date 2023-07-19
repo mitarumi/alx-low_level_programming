dc0fc0f0e15f2375abe0a91d3f85fca8d49ef1a6
Oladipo Victor <dipovictor07@gmail.com>
2023-07-19 16:20:19 -0700

    commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,15 +6,21 @@
  *
  * Return: Always 0.
  */
+void print_alphabet(void);
 
 int main(void)
+{
+	print_alphabet();
+	putchar('\n');
+	return (0);
+}
+
+void print_alphabet(void)
 {
 	char alphabet;
 
 	for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
+	{
 		putchar(alphabet);
-
-	putchar('\n');
-	return (0);
-
+	}
 }
