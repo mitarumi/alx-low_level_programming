61b4c689a49723d282931cbd9c7e6a8d6b1072e5
Oladipo Victor <dipovictor07@gmail.com>
2023-07-24 16:08:48 -0700

    commit

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,11 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 
-/* swap function - for swapping */
+// swap_int : swap two numbers
 void swap_int(int *a, int *b)
 {
 	int temp;
-	temp = *a
-	*a = *b
-	*b = temp
+	temp = *a;
+	*a = *b;
+	*b = temp;
 }
