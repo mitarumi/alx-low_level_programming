888371a32da44ba9f34b17003ede05d15edea14f
Oladipo Victor <dipovictor07@gmail.com>
2023-07-25 14:53:42 -0700

    commit

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,11 +1,18 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * _strlen - show the length of a string
+ *
+ * @S: string
+ *
+ * Return: lenth of string
+ */
 int _strlen(char *s)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; ++i)
+	for (i = 0; *s != '\0'; i++; s++)
 
 	return (i);
 
